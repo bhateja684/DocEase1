3bd8dbfea6cffee50d37561b1ed2d5447584d4a0
Aditya Bhateja <145838337+bhateja684@users.noreply.github.com>
2025-03-21 22:42:26 +0530

    added comment

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./components/AuthContext";
 import { motion, AnimatePresence } from "framer-motion";
-
+//xwmchmvwhxwc
 // Pages
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
